5cba502e4b9fd0ffeb69b69b2a8ef11f256cc2e2
aaronlyc <blockchain_lyc@163.com>
2020-10-20 16:58:26 +0800

    modify the sort list

--- sort7/4_insertSort_test.go
+++ sort7/4_insertSort_test.go
@@ -6,35 +6,23 @@ import (
 	"time"
 )
 
-func Test_heapSort(t *testing.T) {
+func Test_insertSort(t *testing.T) {
 	type args struct {
 		data []int
-		a    int
-		b    int
 	}
 	tests := []struct {
 		name string
 		args args
 	}{
-		{
-			name: "test1",
-			args: args{
-				data: []int{10, 7, 6, 8, 9, 5, 4},
-				a:    0,
-				b:    7,
-			},
-		},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Logf("the original nums is: %v\n", tt.args.data)
-			heapSort(tt.args.data, tt.args.a, tt.args.b)
-			t.Logf("the sorted nums is: %v\n", tt.args.data)
 		})
 	}
 }
 
-func Benchmark_heapSort(b *testing.B) {
+func BenchmarkInsertSort(b *testing.B) {
 	rand.Seed(time.Now().UnixNano())
 	var testSlice [10000]int
 	for i := 0; i < 10000; i++ {
@@ -45,6 +33,6 @@ func Benchmark_heapSort(b *testing.B) {
 	b.ResetTimer()
 
 	b.Run("test1", func(b *testing.B) {
-		heapSort(test, 0, 10000)
+		insertionSort(test, 0, 10000)
 	})
 }
